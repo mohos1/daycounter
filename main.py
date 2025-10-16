91d90b9d6b3a4e68973df2a5fb2c732624aa8803
mohammad hossein <13mr.moh87@gmail.com>
2025-10-16 14:00:54 +0330

    added fixed timezone

--- main.py
+++ main.py
@@ -8,19 +8,26 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = int(os.getenv('CHAT_ID'))
-end_of_year=datetime.date(2026,3,21)
-emojis_dict={'1':'1️⃣','2':'2️⃣','3':'3️⃣','4':'4️⃣','5':'5️⃣','6':'6️⃣','7':'7️⃣','8':'8️⃣','9':'9️⃣','0':'0️⃣'}
+end_of_year = datetime.date(2026, 3, 21)
+emojis_dict = {'1':'1️⃣','2':'2️⃣','3':'3️⃣','4':'4️⃣','5':'5️⃣','6':'6️⃣','7':'7️⃣','8':'8️⃣','9':'9️⃣','0':'0️⃣'}
+
 bot = Bot(token=BOT_TOKEN)
 scheduler = AsyncIOScheduler()
 
 async def send_daily_message():
     today = datetime.date.today()
-    daysleft=''
-    for i in str((end_of_year-today).days):
-        daysleft+=emojis_dict[i]
+    daysleft = ''.join(emojis_dict[i] for i in str((end_of_year - today).days))
     await bot.send_message(chat_id=CHAT_ID, text=f"{daysleft} days left")
 
-scheduler.add_job(lambda:asyncio.create_task(send_daily_message()), 'cron', hour=6, minute=0)
+# Schedule the job
+scheduler.add_job(send_daily_message, 'cron', hour=23, minute=23)
 
-print("Bot started. Waiting to send messages...")
-scheduler.start()
+async def main():
+    scheduler.start()           # Start scheduler
+    print("Bot started. Waiting to send messages...")
+    # Wait indefinitely without while True
+    await asyncio.Event().wait()
+
+# Start the event loop
+asyncio.run(main())
+
